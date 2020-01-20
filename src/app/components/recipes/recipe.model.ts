84db2d07cc98cd84d7d4bfc2cda2e82874cc2ae0
Caio Henrique Zavitoski <chz55@hotmail.com>
2020-01-20 13:04:49 -0300

    Adicionando modelo de recipes

--- src/app/components/recipes/recipe.model.ts
+++ src/app/components/recipes/recipe.model.ts
@@ -1,9 +1,11 @@
-export class Recipe {
+export class RecipeModel {
   public name: string;
   public description: string;
   public imagePath: string;
 
   constructor(name: string, desc: string, imagePath: string) {
-
+    this.name = name
+    this.description = desc
+    this.imagePath = imagePath
   }
 }
